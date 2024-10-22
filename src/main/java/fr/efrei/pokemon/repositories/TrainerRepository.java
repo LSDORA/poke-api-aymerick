677ea7cfe62f3332d89d568a51d2d540b7762e6b
Aymerick <120599499+LSDORA@users.noreply.github.com>
2024-10-22 12:59:43 +0200

    voila

--- src/main/java/fr/efrei/pokemon/repositories/TrainerRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.efrei.pokemon.repositories;
-
-import fr.efrei.pokemon.models.Trainer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TrainerRepository extends JpaRepository<Trainer, String> {
-}
